f0e0eb8ba21aa115c671d9df6b4ff4b6535d3cc9
wrestle-R <russeldanielpaul@gmail.com>
2026-01-16 15:53:54 +0530

    Add protected routes for student and teacher authentication; enhance login context handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import TeacherDashboard from "@/pages/Teacher/TeacherDashboard";
 import {Toaster} from '@/components/ui/sonner';
 import { StudentProvider } from './context/StudentContext';
 import { TeacherProvider } from './context/TeacherContext';
+import { StudentProtectedRoute, TeacherProtectedRoute } from './components/ProtectedRoute';
 
 const App = () => {
   return (
@@ -19,10 +20,17 @@ const App = () => {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Landing />} />
+              
               <Route path="/student/login" element={<StudentAuth />} />
+              <Route element={<StudentProtectedRoute />}>
+                 <Route path="/student/dashboard" element={<StudentDashboard />} />
+              </Route>
+              
               <Route path="/teacher/login" element={<TeacherAuth />} />
-              <Route path="/student/dashboard" element={<StudentDashboard />} />
-              <Route path="/teacher/dashboard" element={<TeacherDashboard />} />
+              <Route element={<TeacherProtectedRoute />}>
+                  <Route path="/teacher/dashboard" element={<TeacherDashboard />} />
+              </Route>
+
               <Route path="*" element={<NotFound />} />
             </Routes>
           </BrowserRouter>
