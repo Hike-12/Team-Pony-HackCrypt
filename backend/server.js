5e4f2a2e51c3b514fe0d8a93e020224658af1e85
Romeiro <theromeirofernandes@gmail.com>
2026-01-16 12:40:29 +0530

    backend initialized

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
