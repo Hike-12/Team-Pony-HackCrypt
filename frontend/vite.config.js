8f6823e9db4daee32ed4c87d1cee3d64f94baea2
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2026-01-17 09:09:16 +0530

    run build again said 4.1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -37,6 +37,9 @@ export default defineConfig({
             purpose: 'any maskable'
           }
         ]
+      },
+      workbox: {
+      maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 // 5 MB
       }
     })
   ],
