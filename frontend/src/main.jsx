db1609701be3ef3fe9c8aa8ae7bfe045731bbdb6
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2026-01-17 09:06:35 +0530

    pwa #2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,13 @@ import { ThemeProvider } from './context/ThemeContext.jsx'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
-  });
+if ('serviceWorker' in navigator) {
+window.addEventListener('load', () => {
+  navigator.serviceWorker.register('/sw.js');
+});
 }
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <App />
