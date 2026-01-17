174016811b56d488c90999cc41f57241f6ee3c47
Hike-12 <160257872+Hike-12@users.noreply.github.com>
2026-01-17 09:04:16 +0530

    pwa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,11 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <App />
